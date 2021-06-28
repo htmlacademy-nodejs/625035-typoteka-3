1ab87bbbc7b5aa9aaf3d972f488c046850d7ca28
Dmitry Svystun <dima1002010@live.com>
2021-06-28 13:32:38 +0300

    module2-task2

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,12 +2,26 @@
 
 const chalk = require(`chalk`);
 const {DEFAULT_COUNT, EXIT_CODE} = require(`../utils/constants`);
+const {readContent} = require(`../utils/utils`);
 const {writeIntoFile, generatePublications} = require(`./utils`);
+
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const run = async (count = DEFAULT_COUNT) => {
   try {
     const countPublication = Number.parseInt(count, 10);
-    const publications = generatePublications(countPublication);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const publications = generatePublications(
+      countPublication,
+      sentences,
+      titles,
+      categories
+    );
+
     await writeIntoFile(publications);
 
     console.info(chalk.green(`[${JSON.stringify(publications[0], null, 2)}]`));
